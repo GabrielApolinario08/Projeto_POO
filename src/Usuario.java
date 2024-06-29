2c5c47506f26f7e60d897b584f1fca3aecee6774
Caio <caiobalieiro676@gmail.com>
2024-06-29 15:35:01 -0300

    UsuarioConstructor

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,3 +1,14 @@
 public class Usuario {
-	
+	protected int codigo;
+	protected String nome, email, senha, tipo;
+
+	public Usuario() {
+		this.codigo = 0;
+		this.nome = "";
+		this.email = "";
+		this.senha = "";
+		this.tipo = "";
+	}
+
+
 }
