5c57ed7c97ccb06b8747f5314ce6bc1fab6c203a
Gabriel Apolinário Fabrício <gabriel.fabricio08@outlook.com>
2024-06-29 15:50:29 -0300

    criacao das classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Caiao gay!");
-        System.out.println("poli gay");
-        System.out.println("AADASDASDASDAS");
+
     }
 }
