ee53bd30f6d40f4a6a1791cb197873e51b7c2c94
carrrllos <caedu762@gmail.com>
2024-06-29 19:23:12 -0300

    JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,30 @@ import entity.Controle;
 
 import java.io.*;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Controle controle = new Controle();
         int opc;
-
+        
         do {
-            System.out.println("------- Menu ---------");
-            System.out.println("Selecione as opções:");
-            System.out.println("1) Entrar");
-            System.out.println("2) Cadastrar");
-            System.out.println("3) Sair");
-            System.out.print("Informe a opção:");
-            opc = entrada.nextInt();
+            opc = Integer.parseInt(JOptionPane.showInputDialog("------- Menu ---------" 
+            + "\nSelecione as opções:"
+            + "\n1) Entrar"
+            + "\n2) Cadastrar"
+            + "\n3) Sair"
+            + "\nInforme a opção:"));
+ 
 
             switch (opc) {
                 case 1:
                 case 2:
-                    System.out.println("Selecione como deseja se cadastrar:");
-                    System.out.println("1) Usuário");
-                    System.out.println("2) Profissional");
-                    controle.cadastrar(entrada.nextInt());
+                   01
+                    controle.cadastrar(Integer.parseInt(JOptionPane.showInputDialog("Selecione como deseja se cadastrar:"
+                    		+ "\n1) Usuário"
+                    		+ "\n2) Profissional")));
                 case 3:
                     System.out.println("Fim do programa!");
                     return;
--- src/entity/Controle.java
+++ src/entity/Controle.java
@@ -1,13 +1,37 @@
 package entity;
 
-public class Controle {
-    Usuario usuario;
+import java.util.Scanner;
 
+public class Controle {
+    Usuario usuario = new Usuario();
+    
+    Scanner sc = new Scanner(System.in);
+    Scanner scStr = new Scanner(System.in);
+    String senha, confSenha;
     public boolean cadastrar(int opc) {
         if (opc == 1) {
             System.out.println("Cadastrar usuário:");
             System.out.print("Nome: ");
-
+            usuario.setNome(scStr.nextLine());
+            System.out.println("Email: ");
+            usuario.setEmail(scStr.nextLine());
+            do{
+            	//while
+            	System.out.println("Digite a senha: ");
+            	senha = sc.nextLine();
+            	System.out.println("Confirme a senha a senha: ");
+            	confSenha = sc.nextLine();
+            	
+            	if(!senha.equals(confSenha)) {
+            		System.out.println();
+            		System.out.println("------------------------------------");
+            		System.out.println("Senhas diferentes, repita a senha!");
+            		System.out.println();
+            	}
+            	
+            }while(senha.equals(confSenha));
+            usuario.setSenha(sc.nextLine());
+            
         }
         return true;
     }
