b34ef67f672a9b611ef05d8c6698665cc51bcfce
Caio <caiobalieiro676@gmail.com>
2024-06-29 16:30:04 -0300

    alteracao 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opc;
 
-        System.out.println("Caiao gay!");
-        System.out.println("Caiao gay!");
-        System.out.println("Caiao gay!");
-        System.out.println("Caiao gay!");
-        System.out.println("poli gay");
+        do {
+            System.out.println("------- Menu ---------");
+            System.out.println("Selecione as opções:");
+            System.out.println("1) Entrar");
+            System.out.println("2) Cadastrar");
+            System.out.println("3) Sair");
+            opc = entrada.nextInt();
+
+            switch (opc) {
+                case 1:
+                case 2:
+                case 3:
+                    System.out.println("Fim do programa!");
+                    return;
+                default:
+                    System.out.println("Selecione uma opção válida.");
+            }
+        } while (true);
+
+
     }
 }
