fe4426274ff0236e81fb2e245a8fdbd174c9f41e
carrrllos <caedu762@gmail.com>
2024-06-29 15:30:42 -0300

    asdads

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Caiao gay!");
         System.out.println("SALVCE");
+        System.out.println("SALVCEadasda");
+        
     }
 }
