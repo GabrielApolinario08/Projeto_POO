6429619d2c154070af98b4c26d769f9dd5ca4386
Caio <caiobalieiro676@gmail.com>
2024-06-29 16:56:54 -0300

    controle 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import entity.Controle;
+
 import java.io.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        Controle controle = new Controle();
         int opc;
 
         do {
@@ -11,11 +14,16 @@ public class Main {
             System.out.println("1) Entrar");
             System.out.println("2) Cadastrar");
             System.out.println("3) Sair");
+            System.out.print("Informe a opção:");
             opc = entrada.nextInt();
 
             switch (opc) {
                 case 1:
                 case 2:
+                    System.out.println("Selecione como deseja se cadastrar:");
+                    System.out.println("1) Usuário");
+                    System.out.println("2) Profissional");
+                    controle.cadastrar(entrada.nextInt());
                 case 3:
                     System.out.println("Fim do programa!");
                     return;
